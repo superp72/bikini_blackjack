51e8178c95b165d329a825537b7ddfad12e91942
Paul Peterson <paulpeterson@192-168-1-10.tpgi.com.au>
2016-06-12 01:50:38 +1000

    random card && found a suit

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
- 
+//Dealing a card
+
+ var randomCard = Math.floor(Math.random()*52+1);
+console.log("I got the card "+randomCard);
+
+var score = Math.floor(Math.random()*52+1);
+
+function findSuit() {
+    if (score <= 13){return "Hearts";}
+    else if(score <= 26){return "Diamonds";}
+    else if(score <= 39){return "Clubs";}
+    else {return "Spades";}
+}
+
+var card = score % 13;
+
+console.log("You got a " + card + " of " + findSuit());
+
+//still need to work out say "12 of Hearts"
